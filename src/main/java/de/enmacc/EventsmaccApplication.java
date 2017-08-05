aa99fc5079b28dd2ae44925d6a006329b4efc64d
bartzilla <ciprianosanchez@gmail.com>
2017-08-05 22:09:15 +0200

    Test intellij git integration

--- src/main/java/de/enmacc/EventsmaccApplication.java
+++ src/main/java/de/enmacc/EventsmaccApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventsmaccApplication
 {
+
 	public static void main(String[] args)
     {
 		SpringApplication.run(EventsmaccApplication.class, args);
