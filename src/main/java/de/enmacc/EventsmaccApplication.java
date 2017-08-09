243a857ad6456c2ba5bb0f47ac0124baabb10531
bartzilla <ciprianosanchez@gmail.com>
2017-08-09 16:34:05 +0200

    formatting

--- src/main/java/de/enmacc/EventsmaccApplication.java
+++ src/main/java/de/enmacc/EventsmaccApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventsmaccApplication
 {
-
 	public static void main(String[] args)
     {
 		SpringApplication.run(EventsmaccApplication.class, args);
