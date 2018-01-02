f7946e94877fd54c8125c1d20038de86f17ef5e9
bartzilla <ciprianosanchez@gmail.com>
2018-01-02 09:56:52 +0100

    removed unused imports

--- src/test/java/de/enmacc/EventsmaccApplicationTests.java
+++ src/test/java/de/enmacc/EventsmaccApplicationTests.java
@@ -1,9 +1,7 @@
 package de.enmacc;
 
-import de.enmacc.data.EventRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
