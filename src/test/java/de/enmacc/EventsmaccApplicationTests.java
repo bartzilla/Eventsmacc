ac3c322924c78757c1ddb54a8614a6c45bbdbce7
bartzilla <ciprianosanchez@gmail.com>
2018-01-03 09:21:28 +0100

    Changed tab indentation to four spaces

--- src/test/java/de/enmacc/EventsmaccApplicationTests.java
+++ src/test/java/de/enmacc/EventsmaccApplicationTests.java
@@ -16,6 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class EventsmaccApplicationTests
 {
 	@Test
-	public void contextLoads() {}
+	public void contextLoads()
+	{
+	}
 
 }
